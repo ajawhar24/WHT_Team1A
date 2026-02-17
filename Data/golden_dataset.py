bdd2f25f2bece51046318596c36056c0b6f3996b
ishimathur <ishimathur002@gmail.com>
2026-02-17 15:46:44 -0500

    Added csv files for RecGym and Gym

---
+++ Data/golden_dataset.py
@@ -0,0 +1,4 @@
+# We want parent classes --- 
+# Dumbbell Arm Curl: Preacher Curl, Barbell Curls, Reverse Barbell Curls
+# Bench Press: Incline barbel bench press, decline barbell bench press, flat dumbbell bench press,
+#               incline dumbbell bench press, decline dumbbell bench press
